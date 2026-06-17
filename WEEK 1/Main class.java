7d2565e5a87b6aa82fed589fc5cf06b7b5608d1a
DharanyaG <gdharanya666@gmail.com>
2026-06-17 23:13:30 +0530

    Add files via upload

---
+++ WEEK 1/Main class.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape s1 = factory.getShape("CIRCLE");
+        s1.draw();
+
+        Shape s2 = factory.getShape("RECTANGLE");
+        s2.draw();
+    }
+}
